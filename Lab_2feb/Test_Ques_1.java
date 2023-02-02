89c1231dd02bb77aaeed47266a35cad7f303693a
Mukul0906 <122787724+Mukul0906@users.noreply.github.com>
2023-02-02 18:19:41 +0530

    Add files via upload

---
+++ Lab_2feb/Test_Ques_1.java
@@ -0,0 +1,16 @@
+package Lab_2feb;
+
+public class Test_Ques_1 {
+	public static void main(String[] args) {
+		int i=10; // local variables
+		int j=20;
+		int k=30;
+		int l=40;
+		int m=50;
+		
+	int avg= (i+j+k+l+m)/5; // logic for average
+	
+	System.out.println("Find the average of five numbers : "+i+", "+j+", "+k+", "+l+" ,"+m );
+	System.out.println("Average of above written numbers is : "+avg);
+	
+	}}
---
+++ Lab_2feb/Test_Ques_2.java
@@ -0,0 +1,22 @@
+package Lab_2feb;
+
+public class Test_Ques_2 {
+	public static void main(String[] args) {
+		int a=10; // local variables
+		int b=5;
+				
+		System.out.println("Before Swapping the numbers are :");
+		
+        System.out.println("First number = " + a);
+        System.out.println("Second Number = " + b);
+ 
+        int c = a; // third variable c
+        a = b;
+        b = c;
+ 
+        System.out.println("After swapping the numbers are :"); 
+        System.out.println("First number = " + a); 
+        System.out.println("Second Number = " + b);
+	}
+
+}
