24bb24ec0165dbcc03d7e291c5dffb524c2bf705
Mukul0906 <122787724+Mukul0906@users.noreply.github.com>
2023-02-10 12:09:12 +0530

    Add files via upload

---
+++ Object.java
@@ -0,0 +1,10 @@
+package LabC;
+// Question 1 : How to create object in java, show demo using program.
+public class Object {
+	String a = "Object is created."; // take an ineger
+	public static void main(String[] args) {
+		//create an object
+		Object obj = new Object();
+		System.out.println(obj.a); // to print
+	}
+}
---
+++ Smallest.java
@@ -0,0 +1,30 @@
+package LabC;
+
+import java.util.Scanner;
+	//	Question 2 : Write a Java method to find the smallest number among three numbers. 
+
+public class Smallest{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in); // scanner created
+
+		 int num1,num2,num3;
+	           System.out.println("Enter three numbers:");
+	           // take input number from user
+	           num1=sc.nextInt();
+	           num2=sc.nextInt(); 
+	           num3=sc.nextInt();
+	           // logic for smallest number
+	           if(num1 < num2 && num1 < num3) // if no1 is smallest
+               {
+	              System.out.println("The The smallest number among the three numbers is : "+num1);
+               }
+            else if(num2 < num3) // if no2 is smallest 
+               {
+	               System.out.println("The smallest number among the three numbers is : "+num2);
+               }
+            else // if no3 is smallest
+               {
+	               System.out.println("The Smallest Is"+num3);
+               }
+	}
+}
