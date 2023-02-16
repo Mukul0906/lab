ccd0a098bc5bb7837c04a63e315304d206659988
Mukul0906 <122787724+Mukul0906@users.noreply.github.com>
2023-02-16 18:26:35 +0530

    Add files via upload

---
+++ Lab_19feb/Adder.java
@@ -0,0 +1,15 @@
+package Lab_16feb;
+// Ques1 :
+class Arithmetic { // super class
+	int add(int a,int b) { // method for addition
+		int c=a+b; 
+		return c;
+	}
+}
+public class Adder extends Arithmetic { // sub clss
+	public static void main(String[] args) {
+		Arithmetic num=new Arithmetic(); // object class
+		int sum = num.add(8, 6); // input numbers
+		System.out.println("Addition of two number is: "+sum );		
+	}
+}
